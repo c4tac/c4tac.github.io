d6f2a0131105cb0249ac48b92bdfbc4d9e655ca3
unknown <billdors@example.com>
2024-03-25 19:44:57 -0700

    *

---
+++ lib.js/nav.js
@@ -0,0 +1,16 @@
+function scorllback() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//scroll back to top
+const navbar = document.querySelector('#NavBarDiv');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
